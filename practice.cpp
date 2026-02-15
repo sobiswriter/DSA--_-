5a3754c415d779ecb5b8950d92a68057409c4b68
sobiswriter <sobi5959@gmail.com>
2026-02-15 20:53:41 +0530

    Sry forgot to update the practice book! ;)

--- practice.cpp
+++ practice.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i<n-1; i++) {
-        for (int j =0; j<n-1 ; j++) {
-            if (arr[j] >  arr[j+1]) {
+void BubbleSort(int arr[], int n){
+    for(int i=0; i<n-i-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
@@ -13,18 +13,53 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-int main() {
+//Discovery: How to print the last no in the array using selection Sort. ;)
+void SelectionSort(int arr[], int n){
+    for(int i=0; i<n; i++){
+        int min_idx = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]>arr[min_idx]){
+                min_idx = j;
+            }
+        }
+        if(min_idx!=i){
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        
+        }
+    }
+}
+
+void InsertionSort(int arr[], int n){
+    for(int i=0; i<n; i++){
+        int key = arr[i];
+        int j = i-1;
+        //it's sorta like backtracking yk 
+        while(j>=0 && arr[j]<key){
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1] = key;
+    }
+}
+
+
+
+int main(){
+    cout<<"how many nums again: \n";
     int n;
-    cout << "enter no: ";
-    cin >> n;
+    cin>> n;
     int arr[n];
-    cout << "enter only " << n << " integeres: ";
-    for (int i = 0; i<n; i++) {
-        cin >> arr[i];
+    cout<<"Put an array as big as "<<n<<" nums: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
     }
-    bubbleSort(arr, n);
-    cout << "sorted array: \n";
-    for (int i = 0; i<n; i++) {
-        cout << arr[i] << " ";
+    InsertionSort(arr, n);
+    
+    cout<<"\nHere is ur array Maam: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
     }
+    return 0;
 }
