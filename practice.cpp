b9f6a2a0f8e7fdc978238daab8c5d10f638ead94
sobiswriter <sobi5959@gmail.com>
2026-02-20 20:47:38 +0530

    Sneaky

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+// Sorry for doing this to u but I need a commit ;)
 void BubbleSort(int arr[], int n){
     for(int i = 0; i<n-1; i++){
         for(int j = 0; j<n-1-i; j++){
