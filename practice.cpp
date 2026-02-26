8ebe54c3f636419169239d72676acb8930fa338c
sobiswriter <sobi5959@gmail.com>
2026-02-26 21:57:40 +0530

    well somehow did it, will do everyday

--- practice.cpp
+++ practice.cpp
@@ -1,48 +1,85 @@
 #include <iostream>
 using namespace std;
-// Sorry for doing this to u but I need a commit ;)
-void BubbleSort(int arr[], int n){
-    for(int i = 0; i<n-1; i++){
-        for(int j = 0; j<n-1-i; j++){
-            if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] =  arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
+
+//1. struct a node, it's just new data and * next;
+struct Node{
+    int data;
+    Node* next;
+};
+
+//2. the append func: with headref & data
+void append(Node** head_ref, int new_data){
+    //2.1 make a newnode & point it to data(no need for int tho);
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = NULL; //point to next node i.e. Null;
+    
+    //2.2 if head is Null, make it newnode;
+    if(*head_ref == NULL){
+        *head_ref = new_node;
+        return;
     }
-}
-//Thoday is a Lucky Day baby ;)
-int BinarySearch(int arr[], int n, int t){
-    int high = n-1;
-    int low = 0;
-    while(low <= high){
-        int mid = low + (high - low)/2;
-        
-        if(arr[mid] == t){
-            return mid;
-        } else if (arr[mid] < t){
-            high = mid -1;
-        } else {
-            low  = mid +1;
-        }
+    //2.3 Make a last node & refer it to head;
+    Node* last = *head_ref;
+    
+    //2.3 while nextnode is not Null, move to next node until NULL;
+    while(last->next != NULL){
+        last=last->next;
     }
-    return -1;
+    last->next = new_node;
+
 }
-//No mistakes boys & gs ;)
+
+//3. Print stuff uising pl func, n* node yk
+void printList(Node* node){
+    //3.1 while node is not null, print n->d and move to next n=n->n
+    while(node != NULL){
+        cout<<node->data<<" -> ";
+        node = node->next;
+    }
+    cout<<"NULL";
+}
+
+//4.Main func;
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i = 0; i<n; i++){
-        cin>>arr[i];
-    }
-    BubbleSort(arr, n);
-    for(int i = 0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
-    int t;
-    cin>>t;
-    cout<<"\nThe index is: "<<BinarySearch(arr, n, t);
+    Node* head = NULL;
+    append(&head, 10);
+    append(&head, 20);
+    append(&head, 40);
+    
+    printList(head);
     return 0;
 }
+
+// void append(Node** head_ref, int new_node){
+//     int Node* new_node = new Node();
+//     new_node->data = new_data;
+//     new_node->next = NULL;
+//     if(*head_ref == NULL){
+//         *head_ref = new_node;
+//         return;
+//     }
+//     Node* last = head_ref;
+//     while(last->next != NULL){
+//         last= lat->next;
+//     }
+//     last->next = new_node;
+// }
+
+
+// void append(Node** head_ref, int new_data){
+//     Node* new_node = new Node();
+//     new_node->data = new_data;
+//     new_node->next = NULL;
+    
+//     if(*head_ref == NULL){
+//         *head_ref = new_node;
+//         return;
+//     }
+    
+//     Node* last = head_ref;
+//     while(last->next != NULL){
+//         last = last->next;
+//     }
+//     last->next = new_node;
+// }
