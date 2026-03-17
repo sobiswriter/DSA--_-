7dd715d47432bcb3054eefedf2afea690cf79b22
sobiswriter <sobi5959@gmail.com>
2026-03-17 16:19:11 +0530

    was on a vication

--- practice.cpp
+++ practice.cpp
@@ -1,43 +1,79 @@
 #include <iostream>
 using namespace std;
 
-int q[5];
+int que[5];
 int front = -1;
 int rear = -1;
 
-void enqueue(int x) {
-    if (rear == 4) cout << "Queue is Full!\n";
-    else {
-        if (front == -1) front = 0;
-        q[++rear] = x;
-        cout << "Enqueued " << x << "\n";
+void enqueue(int x){
+    if(rear == 4){
+        cout<<"Que is full\n";
+    } else {
+        if(front == -1) front = 0;
+        que[++rear] = x;
+        cout<<"Qued "<<x<<"\n";
     }
 }
 
 void dequeue(){
-    if(front == -1 || front > rear){
-        cout<<"No one's left\n";
+    if(rear == -1){
+        cout<<"It's an empty que\n";
     } else {
-        cout<<"Sending "<<q[front++]<<" out\n";
+        cout<<"Dequed "<<que[front++]<<"\n";
     }
 }
 
 void printQueue(){
-    if(front == -1 || front > rear){
-        cout<<"No one to print ;_;";
+    if(rear == -1 || front > rear){
+        cout<<"Empty again\n";
     } else {
-        cout<<"Queue: ";
-        for(int i = front; i <= rear; i++){
-            cout<<q[i]<<" ";
+        cout<<"QueL: ";
+        for(int i = front; i<=rear; i++){
+            cout<<que[i]<<" ";
         }
         cout<<"\n";
     }
 }
 
+// void enqueue(int x){
+//     if (rear == 4){
+//         cout<<"Quw is full\n";
+//     } else {
+//         if (front = 0) front = 1;
+//         que[++rear] = x;
+//         cout<<"Enqued "<<x<<"\n";
+//     }
+// }
+
+// void dequeue(){
+//     if(rear == -1){
+//         cout<<"QW is empty\n";
+//     } else {
+//         cout<<"Dequed "<<que[front++]<<"\n";
+//     }
+// }
+
+// void printQueue(){
+//     if(rear == -1 || front > rear){
+//         cout<<"The QW is empty yr\n";
+//     } else {
+//         cout<<"Que: ";
+//         for(int i = front; i<=rear; i++){
+//             cout<<que[i]<<" ";
+//         }
+//         cout<<"\n";
+//     }
+// }
+
 int main(){
+    dequeue();
     enqueue(10);
     enqueue(15);
     enqueue(20);
+    printQueue();
+    enqueue(56);
+    enqueue(57);
+    enqueue(88);
     printQueue();
     dequeue();
     printQueue();
