89758aeaf68fcf9b6eb43251b93c8a01f137f76f
sobiswriter <sobi5959@gmail.com>
2026-03-22 19:34:43 +0530

    fr tmrw

---
+++ UNIT 4/bst.insert.search.transversal.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// INSERT
+Node* insert(Node* root, int val) {
+    if (root == NULL) return new Node(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// SEARCH
+bool search(Node* root, int key) {
+    if (root == NULL) return false;
+    if (root->data == key) return true;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+// TRAVERSALS
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root) {
+    if (root == NULL) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = NULL;
+
+    // inserting values
+    int arr[] = {50, 30, 70, 20, 40, 60, 80};
+    for (int x : arr)
+        root = insert(root, x);
+
+    cout << "Inorder: ";
+    inorder(root);
+
+    cout << "\nPreorder: ";
+    preorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    // search
+    int key = 40;
+    if (search(root, key))
+        cout << "\nFound " << key;
+    else
+        cout << "\nNot Found";
+
+    return 0;
+}
